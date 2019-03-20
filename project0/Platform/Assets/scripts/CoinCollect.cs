cb7cf1da91fe3aeb8abe3385013cfeff9df49298
Raymond Hidalgo <ra8m0ndhidalgo@gmail.com>
2019-03-19 22:43:38 -0400

    Project 0
    Add colliders

---
+++ project0/Platform/Assets/scripts/CoinCollect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollect : MonoBehaviour
+{
+    public Text scoreText;
+    public Slider spidey;
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      scoreText.text = "score = " + score.ToString();
+      spidey.value = score;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D otherObject)
+    {
+       if (otherObject.gameObject.tag == "Pick Up")
+       {
+          score++;
+
+          spidey.value=score;
+
+          if(score == 100)
+          {
+            scoreText.text = "Winner";
+
+          }
+          else
+          {
+            scoreText.text = "score = " + score.ToString();
+          }
+
+          Destroy(otherObject.gameObject);
+          Debug.Log(score);
+       }
+    }
+
+
+}
---
+++ project0/Platform/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public int points = 0;
+    private Rigidbody2D rb;
+    //private Vector2 moreVelocity;
+    int speed = 10;
+
+    private bool faceR= true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float vert = Input.GetAxis("Vertical")*speed;
+      float horz = Input.GetAxis("Horizontal")*speed;
+
+      vert *= Time.deltaTime;
+      horz *= Time.deltaTime;
+
+      transform.Translate(horz,vert,0);
+
+      if (faceR == false && horz > 0)
+      {
+         Flip();
+      }
+      else if (faceR == true && horz < 0)
+      {
+         Flip();
+      }
+    }
+
+  void Flip()
+  {
+    faceR = !faceR;
+    Vector3 Scaler = transform.localScale;
+    Scaler.x *= -1;
+    transform.localScale = Scaler;
+  }
+  
+}

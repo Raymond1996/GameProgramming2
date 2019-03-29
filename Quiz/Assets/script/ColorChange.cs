366645741a659920731cd1934613f6cdbe93383a
Raymond Hidalgo <ra8m0ndhidalgo@gmail.com>
2019-03-28 22:48:48 -0400

    Quiz
    Missing The color change script

---
+++ Quiz/Assets/script/ColorChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    Color myColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnCollider2DEnter(Collision2D other)
+    {
+        myColor=other.gameObject.GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = myColor;
+
+    }
+}
---
+++ Quiz/Assets/script/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+
+        }
+
+     // normalization prevents unnaturally fast diagonal movement
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+}

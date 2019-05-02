13f1543be4d29a1c8372bc901a09149ebb45e06f
Raymond Hidalgo <ra8m0ndhidalgo@gmail.com>
2019-05-01 22:44:18 -0400

    Project02
    Final Project

---
+++ project02/Project02/Assets/Script/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+       Application.LoadLevel(1);
+    }
+    public void OnStartGame()
+    {
+      //SceneManager.LoadScene("Level02");
+    }
+    public void QuitGame()
+    {
+      Debug.Log("QUIT!");
+      Application.Quit();
+    }
+}
